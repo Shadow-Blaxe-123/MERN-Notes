ecc51ef0f6d291d33a24569196e20030af17c5ba
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-14 21:36:43 +0600

    Made empty new route files.

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import NotesFetchrouter from "./routes/NotesFetch.js";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.use("/api/notes", NotesFetchrouter);
 
 export default app;
