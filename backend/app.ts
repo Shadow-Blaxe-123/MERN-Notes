f7236d0de6176cc6b342d69acde38cd5560a3264
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-14 21:57:00 +0600

    Added RateLimiter on the backend.

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import NotesFetchrouter from "./routes/NotesFetch.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+const apiLimiter = rateLimit({
+  max: 5,
+  windowMs: 60 * 1000,
+  statusCode: 429,
+  message: "Too many requests, please try again later.",
+});
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello World!");
 });
 
+app.use("/api", apiLimiter);
 app.use("/api/notes", NotesFetchrouter);
 
 export default app;
